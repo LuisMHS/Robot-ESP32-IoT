f07b41237667055e620d998ef24f4c4051d0e024
Luis Miguel Hernández Samaniego <48294999+LuisMHS@users.noreply.github.com>
2025-04-22 13:23:04 -0500

    Add files via upload

---
+++ examples/Temas Clase Otto/Tema_Robot_IoT/Tema6_Touch/Tema6_Touch.ino
@@ -0,0 +1,61 @@
+/*
+   @Sketch: Tema6_Touch.ino
+   @author: Luis Miguel Hernández Samaniego
+   Arduino: 2.3.6
+   @version ESP32: 3.2
+*/
+
+//-------------------------------------------------//
+//Incluir el archivo Robot_IoT.h de la librería RobotESP32IoT
+#include <Robot_IoT.h>
+//-------------------------------------------------//
+
+//----------------------Variable-------------------//
+unsigned long t = 0;
+unsigned long tp = 0;
+unsigned long ta = 0;
+bool touch;
+//-------------------------------------------------//
+
+//---------------------Objeto----------------------//
+Robot_IoT ESPBiped;
+//-------------------------------------------------//
+
+void setup() {
+  //-------------------------------------------------//
+  // Configurar el robot ESPBiped
+  ESPBiped.begin();
+  //-------------------------------------------------//
+
+  //-------------------------------------------------//
+  //Configurar la PC comunicación serial a 9600 baud
+  Serial.begin(9600);
+  //-------------------------------------------------//
+
+  //-------------------------------------------------//
+  tp = millis();  //medición del tiempo pasado
+  //-------------------------------------------------//
+}
+
+void loop() {
+  /*
+  ta = millis();                            //lectura del tiempo actual
+  t = (ta - tp);                            //tiempo transcurrido
+  if (t >= 100) {                           //Medir el touch cada 100ms
+    Serial.println(ESPBiped.touch_Read());  //Ver los valor numerico del touch del pin D4
+    tp = ta;                                //Actualizar el tiempo pasado con el tiempo actual
+  }
+  */
+  //-------------------------------------------------//
+  //Obtener el valor promedio al tocar y dejar de tocar
+  touch = ESPBiped.touch(50);
+  //-------------------------------------------------//
+
+  //-------------------------------------------------//
+  while (touch) {
+    //Obtener el valor promedio al tocar y dejar de tocar
+    touch = ESPBiped.touch(50);
+    ESPBiped.walk_Forward(3);  //ESPBiped camina hacia adelante
+  }
+  //-------------------------------------------------//
+}
