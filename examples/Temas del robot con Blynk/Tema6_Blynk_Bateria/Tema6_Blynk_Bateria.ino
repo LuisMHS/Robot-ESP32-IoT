19a7fcf869d5f5b3b10ebc5eb0ccd512f13299ef
Luis Miguel Hernández Samaniego <48294999+LuisMHS@users.noreply.github.com>
2026-03-12 08:34:43 -0700

    Obscure Wi-Fi credentials in Blynk connection
    Replaced Wi-Fi credentials with placeholders for security.

--- examples/Temas del robot con Blynk/Tema6_Blynk_Bateria/Tema6_Blynk_Bateria.ino
+++ examples/Temas del robot con Blynk/Tema6_Blynk_Bateria/Tema6_Blynk_Bateria.ino
@@ -38,7 +38,7 @@ void setup() {
 
   //-------------------------------------------------//
   // Conectar a Blynk con SSID y Contraseña de la red Wi-Fi
-  Blynk.begin(BLYNK_AUTH_TOKEN, "Taller robotica", ".*89LuisUnivLM");
+  Blynk.begin(BLYNK_AUTH_TOKEN, "***********", "***********");
   //-------------------------------------------------//
 
   //-------------------------------------------------//
