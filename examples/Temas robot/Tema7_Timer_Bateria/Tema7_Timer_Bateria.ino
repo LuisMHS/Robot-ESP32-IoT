b35157add5bf73ae8f50719ce794e67d9b37ae02
Luis Miguel Hernández Samaniego <48294999+LuisMHS@users.noreply.github.com>
2026-03-12 08:30:39 -0700

    Update Tema7_Timer_Bateria.ino

--- examples/Temas robot/Tema7_Timer_Bateria/Tema7_Timer_Bateria.ino
+++ examples/Temas robot/Tema7_Timer_Bateria/Tema7_Timer_Bateria.ino
@@ -1,1 +1,45 @@
+/*
+   @Sketch: Tema7_Timer_Bateria.ino
+   @Author: Luis Miguel Hernández Samaniego
+   @Arduino: 2.3.8
+   @Version ESP32: 3.1.1
+   @Fecha: 12/03/2026 
+   @Placa: ESP32 WROOM genérico
+   @Version código:0.0.2
+*/
+//-------------------------------------------------//
+//Incluir el archivo Robot_IoT.h de la librería RobotESP32IoT
+#include <Robot_IoT.h>
+//-------------------------------------------------//
+
+//---------------------Objeto----------------------//
+Robot_IoT ESP32biped;
+//-------------------------------------------------//
+
+void setup() {
+  //-------------------------------------------------//
+  //Configurar la comunicación serial a 9600 baudios
+  Serial.begin(9600);
+  //-------------------------------------------------//
+
+  //-------------------------------------------------//
+  // Configurar el robot ESP32biped
+  ESP32biped.begin();
+  //-------------------------------------------------//
+
+  //-------------------------------------------------//
+  //Configurar el timer cada 70000ms para que se ejecute la función  program_Battery
+  ESP32biped.attach_ms(700, program_Battery);
+  //-------------------------------------------------//
+}
+
+void loop() {
+}
+// Función program_Battery para medir el voltaje y monitorear la batería
+void program_Battery() {
+  Serial.print("Voltaje en el pin D35=");
+  Serial.println(ESP32biped.voltage_Read());
+  Serial.print("Voltaje en la batería=");
+  Serial.println(ESP32biped.battery(3.1645));
+}
 
