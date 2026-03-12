e6a625618397648be7492bf62dfafce16c959589
Luis Miguel Hernández Samaniego <48294999+LuisMHS@users.noreply.github.com>
2026-03-12 08:32:00 -0700

    Update Tema8_Flash.ino

--- examples/Temas robot/Tema8_Flash/Tema8_Flash.ino
+++ examples/Temas robot/Tema8_Flash/Tema8_Flash.ino
@@ -1,1 +1,54 @@
+/*
+   @Sketch: Tema8_Flash.ino
+   @Author: Luis Miguel Hernández Samaniego
+   @Arduino: 2.3.8
+   @Version ESP32: 3.1.1
+   @Fecha: 12/03/2026 
+   @Placa: ESP32 WROOM genérico
+   @Version código:0.0.2
+*/
+
+//----------------------Header---------------------//
+//Incluir el archivo Preferences.h
+#include <Preferences.h>
+//-------------------------------------------------//
+
+//----------------------Variable-------------------//
+int angleLeftLeg;
+int angleRightLeg;
+int angleLeftFoot;
+int angleRightFoot;
+//-------------------------------------------------//
+
+//---------------------Objeto----------------------//
+Preferences preferences;
+//-------------------------------------------------//
+
+void setup() {
+  //-------------------------------------------------//
+  //Configurar la comunicación serial a 9600 baud
+  Serial.begin(115200);
+  //-------------------------------------------------//
+
+  //-------------------------------------------------//
+  //Obtener los ángulos guardados
+  preferences.begin("Angulos", false);
+  angleLeftLeg = preferences.getInt("LeftLeg", 90);
+  angleRightLeg = preferences.getInt("RightLeg", 90);
+  angleLeftFoot = preferences.getInt("LeftFoot", 90);
+  angleRightFoot = preferences.getInt("RightFoot", 90);
+  preferences.end();
+  //-------------------------------------------------//
+}
+
+void loop() {
+  //-------------------------------------------------//
+  //Ángulos guardados
+  Serial.println(angleLeftLeg);
+  Serial.println(angleRightLeg);
+  Serial.println(angleLeftFoot);
+  Serial.println(angleRightFoot);
+  delay(500);
+  //-------------------------------------------------//
+}
 
